0379fc71a38f9c04bfd8a95494231b8d0d9ef136
Shail Shouryya <shailshouryya@gmail.com>
2019-11-16 14:51:19 -0800

    source file, say comment line for repeat use

--- scripts/001_gitcompletion_setup.bash
+++ scripts/001_gitcompletion_setup.bash
@@ -1,7 +1,11 @@
 # downloads git-completion for core git bash/zsh script from the Git master repo and adds file to your bash_profile so you don't need to manually activate in every new terminal session
 
 # run using:
-# 001_gitcompletion_setup.bash 
+# 001_gitcompletion_setup.bash
 
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > ~/.git_completion.bash
+
+# comment out the following line if you already ran this and just want to update git-completion from the downstream master repository
 touch ~/.bash_profile && printf "\nsource ~/.git-completion.bash\n" >> ~/.bash_profile
+
+source ~/.bash_profile
