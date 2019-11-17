1faa888523911f8adaefcd5d4dcaac1ae48c9781
Shail Shouryya <shailshouryya@gmail.com>
2019-11-16 16:20:08 -0800

    clarifying rerunning script

--- scripts/001_gitcompletion_setup.bash
+++ scripts/001_gitcompletion_setup.bash
@@ -2,10 +2,11 @@
 
 # run using:
 # 001_gitcompletion_setup.bash
+# you can rerun this script to update the git-completion file from master, just comment out the indicated line below
 
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > ~/.git_completion.bash
 
-# comment out the following line if you already ran this and just want to update git-completion from the downstream master repository
+# comment out THIS following line if you already ran this and just want to update git-completion from the downstream master repository
 touch ~/.bash_profile && printf "\nsource ~/.git-completion.bash\n" >> ~/.bash_profile
 
 source ~/.bash_profile
