c73aafed7e83a9af04241fdd9f05dec83a62251c
Shail Shouryya <42100758+Shail-Shouryya@users.noreply.github.com>
2019-11-17 01:44:53 -0800

    changed spacing to increase readability on GitHub

--- scripts/001_gitcompletion_setup.bash
+++ scripts/001_gitcompletion_setup.bash
@@ -1,4 +1,5 @@
-# downloads git-completion for core git bash/zsh script from the Git master repo and adds file to your bash_profile so you don't need to manually activate in every new terminal session
+# downloads git-completion for core git bash/zsh script from the Git master repo and adds file to 
+# your bash_profile so you don't need to manually activate in every new terminal session
 
 # run using:
 # 001_gitcompletion_setup.bash
