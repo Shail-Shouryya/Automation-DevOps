2f821dccf3bb07dfe36af671c1d9d579b0876af1
Shail Shouryya <42100758+Shail-Shouryya@users.noreply.github.com>
2019-11-21 22:41:08 -0800

    changes underscore to hyphen

--- scripts/001_gitcompletion_setup.bash
+++ scripts/001_gitcompletion_setup.bash
@@ -5,7 +5,7 @@
 # 001_gitcompletion_setup.bash
 # rerun this script to update git-completion file from master, commenting out indicated line
 
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > ~/.git_completion.bash
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > ~/.git-completion.bash
 
 # comment out THIS following line if you already ran this and just want to update git-completion from the downstream master repository
 touch ~/.bash_profile && printf "\nsource ~/.git-completion.bash\n" >> ~/.bash_profile
