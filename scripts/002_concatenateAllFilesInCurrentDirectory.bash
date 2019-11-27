81c976b30b744a9519b4facb5a7f013b1e30f9b0
Shail Shouryya <shailshouryya@gmail.com>
2019-11-26 18:26:39 -0800

    adds variables to make script more dynamic

--- scripts/002_concatenateAllFilesInCurrentDirectory.bash
+++ scripts/002_concatenateAllFilesInCurrentDirectory.bash
@@ -1,6 +1,11 @@
-for file in $(pwd)/*; do
-    echo $file;
-    cat $file;
-    printf '*==*%.0s' {1..30};
-    printf "\n\n";
+FILE_NAME=yourFileName.someExtension
+DIRECTORY=$(pwd)
+# to hard code path:
+# DIRECTORY=path/to/directory
+
+for file in $DIRECTORY/*; do
+    echo $file >> $FILE_NAME;
+    cat $file >> $FILE_NAME;
+    printf '*==*%.0s' {1..30} >> $FILE_NAME;
+    printf "\n\n" >> $FILE_NAME;
 done
