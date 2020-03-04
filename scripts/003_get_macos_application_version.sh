4baa1a166017ebf4b0c6e3b59e3e0883cc583088
Shail Shouryya <shailshouryya@gmail.com>
2020-03-04 01:01:32 -0800

    prints options for <YOUR_APPLICATION> to terminal

--- scripts/003_get_macos_application_version.sh
+++ scripts/003_get_macos_application_version.sh
@@ -1,2 +1,8 @@
 APPLICATION_NAME="<YOUR_APPLICATION>" # since the application name is stored in a string, you do NOT need to escape spaces as you would if you were running the following line directly from terminal
 cat /Applications/"${APPLICATION_NAME}".app/Contents/Info.plist | tr '\t' ' ' | tr '\n' ' ' | grep -ie "<key>CFBundle\(\w\+\)\?Version\w\+</key>  <string>[0-9\.]\+" -o
+
+echo
+echo "Options for <YOUR_APPLICATION> include:"
+ls /Applications/
+echo
+echo "NOTE: Make sure you leave out the \".app\" part when changing <APPLICATION_NAME>"
