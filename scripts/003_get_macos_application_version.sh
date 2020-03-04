b8bed662d915ef330f8ede8aaebbf06e23b2a754
Shail Shouryya <shailshouryya@gmail.com>
2020-03-04 00:57:48 -0800

    terminal commands to get version number for any macos app
    - will break on some files (such as Microsoft Word) that encode information in the Info.plist file in binary
      - apparently Safari doesn't follow this convention at all
      - other than that, this works perfectly for most applications

---
+++ scripts/003_get_macos_application_version.sh
@@ -0,0 +1,2 @@
+APPLICATION_NAME="<YOUR_APPLICATION>" # since the application name is stored in a string, you do NOT need to escape spaces as you would if you were running the following line directly from terminal
+cat /Applications/"${APPLICATION_NAME}".app/Contents/Info.plist | tr '\t' ' ' | tr '\n' ' ' | grep -ie "<key>CFBundle\(\w\+\)\?Version\w\+</key>  <string>[0-9\.]\+" -o
