e85ff56bc66ee829c4459c8d40c708bca58512c7
Shail Shouryya <shailshouryya@gmail.com>
2020-03-04 01:10:41 -0800

    clarifies terminal output

--- scripts/003_get_macos_application_version.sh
+++ scripts/003_get_macos_application_version.sh
@@ -1,8 +1,10 @@
-APPLICATION_NAME="YOUR_APPLICATION" # since the application name is stored in a string, you do NOT need to escape spaces as you would if you were running the following line directly from terminal
+APPLICATION_NAME="APPLICATION_NAME" # since the application name is stored in a string, you do NOT need to escape spaces as you would if you were running the following line directly from terminal
+echo "The version number for your currently installed \"${APPLICATION_NAME}\" application is:"
 cat /Applications/"${APPLICATION_NAME}".app/Contents/Info.plist | tr '\t' ' ' | tr '\n' ' ' | grep -ie "<key>CFBundle\(\w\+\)\?Version\w\+</key>  <string>[0-9\.]\+" -o
 
 echo
-echo "Options for YOUR_APPLICATION include:"
+echo "Currently APPLICATION_NAME is set to ${APPLICATION_NAME}"
+echo "Options for APPLICATION_NAME include:"
 ls /Applications/
 echo
 echo "NOTE: Make sure you leave out the \".app\" part when changing APPLICATION_NAME"
